d3df7f1d3cda50fda1cfb1ffcaff22adca2e96c1
Handenur-Kilicarslan <handenur.kilicarslan@bil.omu.edu.tr>
2021-10-12 10:06:15 +0300

    Arragements

--- Openit/Assets/scripts/numbers.cs
+++ Openit/Assets/scripts/numbers.cs
@@ -5,47 +5,48 @@ using UnityEngine.UI;
 
 public class numbers : MonoBehaviour
 {
-  public string number;
-	public string correctNumber;
-	public Animator anim;
-	public Animator anim2;
-	public Animator spinanim;
-	public GameManager gm;
-	public value vle;
-	public Text text;
-	public Text boxtext;
-	public GameObject prize;
+    public string number;
+    public string correctNumber;
+   
+    public Animator anim;
+    public Animator anim2;
+    public Animator spinanim;
+    public GameManager gm;
+    public value vle;
+    public Text text;
+    public Text boxtext;
+    public GameObject prize;
 
-	bool won;
+    bool won;
 
-	private void Start()
-	{
-		correctNumber = Random.Range(1000, 9999).ToString();
-		text.text = correctNumber.ToString();
-		won = false;
-	}
+    private void Start()
+    {
+        correctNumber = Random.Range(1000, 9999).ToString();
+        text.text = correctNumber.ToString();
+        won = false;
+    }
 
-	private void Update()
-	{
-		boxtext.text = number;
-		if(number == correctNumber && won != true)
-		{
-			prize.transform.parent = null;
-			prize.transform.position = GameObject.Find("boxLocation").transform.position;
-			anim.SetBool("open", true);
-			StartCoroutine(_win());
-			won = true;
-		}
-	}
+    private void Update()
+    {
+        boxtext.text = number;
+        if (number == correctNumber && won != true)
+        {
+            prize.transform.parent = null;
+            prize.transform.position = GameObject.Find("boxLocation").transform.position;
+            anim.SetBool("open", true);
+            StartCoroutine(_win());
+            won = true;
+        }
+    }
 
-	IEnumerator _win()
-	{
-		yield return new WaitForSeconds(1.5f);
-		anim2.SetBool("win", true);
-		yield return new WaitForSeconds(0.75f);
-		spinanim.SetBool("win", true);
-		yield return new WaitForSeconds(0.75f);
-		gm.win(vle);
+    IEnumerator _win()
+    {
+        yield return new WaitForSeconds(1.5f);
+        anim2.SetBool("win", true);
+        yield return new WaitForSeconds(0.75f);
+        spinanim.SetBool("win", true);
+        yield return new WaitForSeconds(0.75f);
+        gm.win(vle);
 
-	}
+    }
 }
